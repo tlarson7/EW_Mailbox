1fd6ddcb20cd9e5871c5f8ed0e110b96a3e425a3
EagleWatch <tannerl@eagleinc.com>
2022-02-10 02:06:08 -0600

    SQL view

--- SQL_testing.py
+++ SQL_testing.py
@@ -1,3 +1,12 @@
 # Testing SQL and github
 import sqlite3
-#test 2
+import pyodbc
+
+cnxn = pyodbc.connect('DRIVER={SQL Server}; SERVER=connectwise; DATABASE=cwwebapp_eaglesoft; UID=expensewise; PWD=expensewise;')
+c = cnxn.cursor()
+
+c.execute('SELECT * FROM cwwebapp_eaglesoft.dbo.eagle_CV_configs_w_EW_agreements')
+columns = [column[0] for column in c.description]
+print(columns)
+for row in c:
+    print(row)
