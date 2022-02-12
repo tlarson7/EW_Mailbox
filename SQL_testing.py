d9059f254c236a6194876c5b061d9057e35a445a
EagleWatch <tannerl@eagleinc.com>
2022-02-12 03:42:17 -0600

    Minor SQL syntax changes and print additions

--- SQL_testing.py
+++ SQL_testing.py
@@ -1,16 +1,16 @@
 # Testing SQL and github
 import sqlite3
 import pyodbc
-import Mailbox_Scrape
 
 def update_row():
-    cursor_sqlite_execute.execute('Update Config_Info Set Config_Status,AGR_End_Date,Cancelled_Flag,Company_Name,Num_Contacts = ?,?,?,?,? WHERE CCID = ?',(status,end_date,cancelled,company,num_contacts,CCID))
+    cursor_sqlite_execute.execute('UPDATE Config_Info SET Config_Status = ?, AGR_End_Date = ?, Cancelled_Flag = ?, Company_Name = ?, Num_Contacts = ? WHERE CCID = ?',(status,end_date,cancelled,company,num_contacts,CCID))
 
     cnxn_sqlite.commit()
 
 cnxn_MySQL = pyodbc.connect('DRIVER={SQL Server}; SERVER=connectwise; DATABASE=cwwebapp_eaglesoft; UID=expensewise; PWD=expensewise;')
+cnxn_MySQL2 = pyodbc.connect('DRIVER={SQL Server}; SERVER=connectwise; DATABASE=cwwebapp_eaglesoft; UID=expensewise; PWD=expensewise;')
 cursor_agr_view = cnxn_MySQL.cursor()
-cursor_contact_view = cnxn_MySQL.cursor()
+cursor_contact_view = cnxn_MySQL2.cursor()
 
 cnxn_sqlite = sqlite3.connect("EW_Review.db")
 cursor_sqlite_select = cnxn_sqlite.cursor()
@@ -28,6 +28,7 @@ for row in cursor_agr_view:
 
     cursor_contact_view.execute('SELECT "Email_Flag" FROM cwwebapp_eaglesoft.dbo.eagle_EW_CV_Contacts WHERE "Config_Name" = ?',(config_name))
     num_contacts = len(cursor_contact_view.fetchall())
+    print(config_name,"|",status,"|",end_date)
 
     #TODO Add initial select check so not to add unneeded loop
     cursor_sqlite_select.execute('SELECT "CCID" FROM "Config_Info" WHERE CCID = ?',(config_name,))
@@ -38,6 +39,7 @@ for row in cursor_agr_view:
         update_row()
 
     else:
+        print("Starting Else")
         cursor_sqlite_select.execute('SELECT "CCID" FROM "Config_Info"')
         for row_sqlite in cursor_sqlite_select:
             CCID = row_sqlite[0]
